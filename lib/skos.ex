1b844ec3790bf6c7fdd60a39c445891e0f8da659
Marcel Otto <marcelotto@gmx.de>
2023-04-20 23:38:27 +0200

    Initial commit
    $ mix new skos --module SKOS
    
    with Elixir v1.4.3

---
+++ lib/skos.ex
@@ -0,0 +1,18 @@
+defmodule SKOS do
+  @moduledoc """
+  Documentation for `SKOS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SKOS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/skos_test.exs
@@ -0,0 +1,8 @@
+defmodule SKOSTest do
+  use ExUnit.Case
+  doctest SKOS
+
+  test "greets the world" do
+    assert SKOS.hello() == :world
+  end
+end
