0bdb687265a565c49542a8d1f1a0c4dc601bfe3a
Marcel Otto <marcelotto@gmx.de>
2024-08-07 12:19:15 +0200

    Add docs

--- lib/skos.ex
+++ lib/skos.ex
@@ -2,6 +2,16 @@ defmodule SKOS do
   @moduledoc """
   Implementation of the Simple Knowledge Organization System (SKOS) vocabulary.
 
+  The following features are provided:
+
+  - The `SKOS` module acts as the `RDF.Vocabulary.Namespace` for the SKOS vocabulary.
+  - Grax structures for all SKOS classes and their properties are defined, namely:
+    - `SKOS.Concept`
+    - `SKOS.ConceptScheme`
+    - `SKOS.Collection`
+
+  Note: `skos:OrderedCollection` is not yet supported.
+
   See <http://www.w3.org/TR/skos-primer>.
   """
 
